101ec4cac00acf0b86bd6d900c7f5fd4827a8390
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-06-18 01:33:26 +0600

    adding like and dislike button

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 # Create your views here.
 class user_register(generic.CreateView):
-    form_class=UserCreationForm #Using Pre-defined User Registration form
+    form_class=UserCreationForm #Using Pre-defined User Registration form We will change
     template_name='registration/register.html'
     success_url=reverse_lazy('login')
 
