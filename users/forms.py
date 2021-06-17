dfc0248982c5634ad5e87c66d9b01f5c08aef136
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-06-18 02:00:37 +0600

    modifying default registration field

---
+++ users/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.db.models.base import Model
+from django.forms import fields
+
+class RegistrationForm(UserCreationForm):
+    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))#Using bootstrap to make it look better
+    firstname=forms.CharField(max_length=250,widget=forms.TextInput(attrs={'class':'form-control'}))
+    lastname=forms.CharField(max_length=250,widget=forms.TextInput(attrs={'class':'form-control'}))
+
+    #Now adding in runtime
+    class Meta:
+        model=User
+        fields=('username','firstname','lastname','email','password1','password2')
+
+    #For changing username and password field appearenc
+    def __init__(self, *args,**kwargs):
+        super(RegistrationForm,self).__init__(*args,**kwargs)
+
+        self.fields['username'].widget.attrs['class']='form-control'
+        self.fields['password1'].widget.attrs['class']='form-control'
+        self.fields['password2'].widget.attrs['class']='form-control'
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
+from users.forms import RegistrationForm
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 # Create your views here.
 class user_register(generic.CreateView):
-    form_class=UserCreationForm #Using Pre-defined User Registration form We will change
+    form_class=RegistrationForm #Using Pre-defined User Registration form We will change
     template_name='registration/register.html'
     success_url=reverse_lazy('login')
 
